9b0c735fea0cb84060a172334bd5a6bd8c557ebc
jmcrook <jmcrook95@gmail.com>
2020-02-15 18:17:08 -0500

    Updates

---
+++ precache-manifest.7de5f9ca463615c0706e03e818ca165b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d1c25f473228de187bb722df1d8118",
+    "url": "/bernie-san/index.html"
+  },
+  {
+    "revision": "e80c33914e47c96c07a5",
+    "url": "/bernie-san/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "232bafcb98dcc84a21da",
+    "url": "/bernie-san/static/js/2.efd80659.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bernie-san/static/js/2.efd80659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80c33914e47c96c07a5",
+    "url": "/bernie-san/static/js/main.9ee19565.chunk.js"
+  },
+  {
+    "revision": "54299f4bdad14c23c6db",
+    "url": "/bernie-san/static/js/runtime-main.db7bcf08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bernie-san/static/media/logo.5d5d9eef.svg"
+  }
+]);
