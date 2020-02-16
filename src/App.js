e3f6f675383f2e9a1eb52b7135d24a12cd687ce1
jmcrook <jmcrook95@gmail.com>
2020-02-15 19:45:06 -0500

    added bernie gifs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import bernie_slow from './bernie_slow.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={bernie_slow} className="App-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Order Now
         </a>
       </header>
     </div>
